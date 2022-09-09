8c3a19aeddf7baf73f39a433e3670d084f14a195
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-09 05:28:11 -0300

    Criador a segunda pagina que será implementado o agendamento

--- venv/paginas/views.py
+++ venv/paginas/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'paginas/index.html'
+
+class AgendamentoView(TemplateView):
+    template_name = 'paginas/agendamento.html'
