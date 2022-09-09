ac5b0521d57d57d42dbb4f20e623a5e02679ca2f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-09-09 08:03:56 -0300

    Criei um app (cadastro) e comecei a configurar models.py para criação de classes e atributos

--- venv/cadastro/models.py
+++ venv/cadastro/models.py
@@ -1,3 +1,34 @@
+from pyexpat import model
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class Campo(models.Model):
+    
+    ESPECIALIDADE = (
+        ('E', 'Ecocardiografista'),
+        ('C', 'Clínico Geral'),
+        ('O', 'Obstetra'),
+    )
+    
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    especialidade = models.CharField(max_length=1, choices=ESPECIALIDADE)
+    
+    def __str__(self) -> str:
+        return f'{self.nome}{self.telefone}{self.email}{self.especialidade}'
+    
+class Endereco(models.Model):
+    
+    cep = models.IntegerField()
+    logradouro = models.IntegerField()
+    numero = models.IntegerField(verbose_name="Número")
+    bairro = models.CharField(max_length=20)
+    cidade = models.CharField(max_length=20)
+    estado = models.CharField(max_length=2)
+    
+    campo = models.ForeignKey(Campo, on_delete=models.PROTECT)
+    
+    def __str__(self) -> str:
+        return f'{self.nome}, ({self.campo})'
